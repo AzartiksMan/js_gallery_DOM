b8785a8ae657f3e3df9fb8dfd7faa79b5aa56cfb
AzartiksMan <azartofgod@gmail.com>
2025-06-18 19:36:58 +0300

    Add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,13 @@
 'use strict';
+
+const thumbs = document.getElementById('thumbs');
+
+thumbs.addEventListener('click', (action) => {
+  action.preventDefault();
+
+  const link = action.target.closest('a');
+
+  const largeImg = document.getElementById('largeImg');
+
+  largeImg.src = link.href;
+});
